86fde7e190c2f437142fb3c872905301f34be301
richard <richardlm57@gmail.com>
2015-05-01 19:24:07 -0430

    Hay que arreglar el match

--- etapa1.rb
+++ etapa1.rb
@@ -1,3 +1,14 @@
+=begin
+
+	@title	Anlisis Lexicográfico
+
+	@author	Richard Lares 11-10508
+	@author	Oscar Guillen 11-11264
+
+	@description
+
+=end
+
 name = ARGV[0]
 file = File.open(name,"r")
 content = file.read
@@ -8,7 +19,7 @@ strings_dic = {"{"=>"LCURLY","}"=>"RCURLY","|"=>"PIPE","%"=>"PERCENTAGE","!"=>"E
 				"<_>"=>"CANVAS","<->"=>"CANVAS","="=>"EQUALS",";"=>"SEMICOLON",
 				"read"=>"READ","write"=>"WRITE","?"=>"QUESTION MARK",
 				":"=>"COLON","("=>"LPARENTHESIS",")"=>"RPARENTHESIS",
-				"\d"=>"NUMBER","+"=>"PLUS","-"=>"MINUS","*"=>"TIMES","/"=>"DIVIDE",
+				"+"=>"PLUS","-"=>"MINUS","*"=>"TIMES","/"=>"DIVIDE",
 				"\/\\"=>"AND","\\\/"=>"OR","^"=>"NOT","<"=>"LESS","<="=>"LESS EQUAL",
 				">"=>"GREATER",">="=>"GREATER EQUAL",
 				"\/="=>"NOTEQUAL","#"=>"EMPTYCANVAS","$"=>"DOLLAR","'"=>"APOSTROPHE","true"=>"TRUE",
@@ -19,26 +30,48 @@ variable_identifiers=Array.new
 i=1
 #string methods: each_char,match | matchdata methods: pre_match, post_match
 content.each_line do |x|
+
 	j=1	
 	#Change order to recognize complex symbols first
-	m=x.match(/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=
-			|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\d|\+|-|\*|\/|\^|<|>
-			|#|\$|'|true|false/)
-	while m do
-		case m[0]
-		when /\/\\|\\\/|<=|>=|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\d
-			|\+|-|\*|\/|\^|<|>|#|\$|'|true|false/
-			correct_program.push("token "+strings_dic[m[0]]+" value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
-		when /\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >/
-			puts "wait"
-		else
-			puts "wait again"
+	s=x.split
+	print s
+	s.each do |t|
+		puts "\nsplit"
+		puts t
+		m=t.match(/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=
+				|([A-Z]|[a-z]|\_)([A-Z]|[a-z]|_|[0-9])*|[0-9]+
+				|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\+|-|\*|\/|\^|<|>
+				|#|\$|'|true|false/)
+		while m do
+			puts "match"
+			puts m[0]
+			puts "postmatch"
+			puts m.post_match
+			if m[0] =~ /\/\\|\\\/|<=|>=|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)
+				|\+|-|\*|\/|\^|<|>|#|\$|'|true|false/
+				correct_program.push("token "+strings_dic[m[0]]+" value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
+
+			elsif m[0] =~ /([A-Z]|[a-z]|\_)([A-Z]|[a-z]|_|[0-9])*/
+				correct_program.push("token IDENTIFIER value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
+
+			elsif m[0] =~ /[0-9]+/
+				correct_program.push("token NUMBER value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
+
+
+			elsif m[0] =~ /\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >/
+				puts "wait"
+
+			else
+				puts "wait again"
+			end
+
+			j+=m[0].size
+			temp=m.post_match
+			m=temp.match(/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=
+				|([A-Z]|[a-z]|\_)([A-Z]|[a-z]|_|[0-9])*|[0-9]+
+				|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\+|-|\*|\/|\^|<|>
+				|#|\$|'|true|false/)
 		end
-		j+=m[0].size
-		temp=m.post_match
-		m=temp.match(/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=
-			|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\d|\+|-|\*|\/|\^|<|>
-			|#|\$|'|true|false/)
 	end
 	i+=1	
 end
