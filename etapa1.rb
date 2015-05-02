cb1c839d6c85349ecaf03f60ed3175ac9417fd6b
richard <richardlm57@gmail.com>
2015-05-01 23:53:02 -0430

    Numeros de columnas

--- etapa1.rb
+++ etapa1.rb
@@ -31,41 +31,48 @@ i=1
 #string methods: each_char,match | matchdata methods: pre_match, post_match
 content.each_line do |x|
 
-	j=1	
+	j=0	
 	#Change order to recognize complex symbols first
 	s=x.split
-	print s
+	#print s
 	s.each do |t|
-		puts "\nsplit"
-		puts t
+		#puts "\nsplit"
+		#puts t
+		while x[j]!=t[0] do
+			j+=1
+		end
 		m=t.match(/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=|
 				|([A-Z]|[a-z]|_)([A-Z]|[a-z]|_|[0-9])*|[0-9]+|
 				|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\+|-|\*|\/|\^|<|>|
 				|#|\$|'|true|false/)
 		while m do
-			puts "match"
-			puts m[0]
-			puts "postmatch"
-			puts m.post_match
-			if m[0] =~ /\/\\|\\\/|<=|>=|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)
+			#puts "match"
+			#puts m[0]
+			#puts "postmatch"
+			#puts m.post_match
+			if m[0] =~ /\/\\|\\\/|<=|>=|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|
 				|\+|-|\*|\/|\^|<|>|#|\$|'|true|false/
-				correct_program.push("token "+strings_dic[m[0]]+" value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
+				correct_program.push("token "+strings_dic[m[0]]+" value ("+m[0]+") at line: "+i.to_s+", column: "+(j+1).to_s)
+				j+=m[0].size
 
-			elsif m[0] =~ /([A-Z]|[a-z]|\_)([A-Z]|[a-z]|_|[0-9])*/
-				correct_program.push("token IDENTIFIER value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
+			elsif m[0] =~ /([A-Z]|[a-z]|_)([A-Z]|[a-z]|_|[0-9])*/
+				correct_program.push("token IDENTIFIER value ("+m[0]+") at line: "+i.to_s+", column: "+(j+1).to_s)
+				j+=m[0].size
 
 			elsif m[0] =~ /[0-9]+/
-				correct_program.push("token NUMBER value ("+m[0]+") at line: "+i.to_s+", column: "+j.to_s)
-
+				correct_program.push("token NUMBER value ("+m[0]+") at line: "+i.to_s+", column: "+(j+1).to_s)
+				j+=m[0].size
 
 			elsif m[0] =~ /\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >/
-				puts "wait"
+				#puts "wait"
+				j+=m[0].size
 
 			else
-				puts "wait again"
+				#puts "wait again"
+				j+=1
 			end
 
-			j+=m[0].size
+			
 			temp=m.post_match
 			m=temp.match(/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=|
 				|([A-Z]|[a-z]|_)([A-Z]|[a-z]|_|[0-9])*|[0-9]+|
