ad0b5051343bd45661c540e0808b4c17baca7946
richard <richardlm57@gmail.com>
2015-06-07 07:20:25 -0430

    Etapa 3

---
+++ TablaSimbolos/class.rb
@@ -0,0 +1,451 @@
+=begin
+
+	@title	Clases para las producciones del parser de Lanscii
+
+	@author	Richard Lares 11-10508
+	@author	Oscar Guillen 11-11264
+
+	@description Declaración de clases (árboles) para las producciones del parser
+
+=end
+
+# Funcion para imprimir pipes.
+def print_pipe(pipe)
+	for i in 1..pipe
+		print "|  "
+	end
+end
+
+# Símbolo inicial con declaraciones de variables
+class PROGRAM_DECLARE_BODY
+	def initialize(val1,val2)
+		@declare = val1
+		@body = val2
+	end
+
+	def to_s(pipe)
+		@body.to_s(pipe)
+	end
+end
+
+# Símbolo inicial sin declaraciones de variables
+class PROGRAM_BODY
+	def initialize(val1)
+		@body = val1
+	end
+
+	def to_s(pipe)
+		@body.to_s(pipe)
+	end
+end
+
+# Declaración de enteros
+class DECLARE_INT
+	def initialize(val)
+		@inst = val
+	end
+end
+
+# Declaración de booleanos
+class DECLARE_BOOL
+	def initialize(val)
+		@inst = val
+	end
+end
+
+# Declaración de lienzos
+class DECLARE_LIE
+	def initialize(val)
+		@inst = val
+	end
+end
+
+# Más identificadores dentro de una declaración
+class MORE_IDENTS
+	def initialize(val1,val2)
+		@id = val1
+		@next_inst = val2
+	end
+end
+
+# Otra declaración de variable
+class IDENTS_DECLARE
+	def initialize(val1,val2)
+		@id = val1
+		@declare = val2
+	end
+end
+
+# Única declaración de variable
+class IDENTS_ID
+	def initialize(val1)
+		@id = val1
+	end
+end
+
+# Asignación
+class BODY_ASSIGN
+
+	def initialize(val1,val2)
+		@id = val1
+		@exp = val2
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "ASSIGN:"
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "VARIABLE:"
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe-1
+		puts "IDENTIFIER: " + @id.to_s
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "EXPRESSION:"
+		@exp.to_s(pipe)
+	end
+end
+
+# Otro cuerpo
+class BODY
+	def initialize(val1)
+		@body = val1
+	end
+
+	def to_s(pipe)
+		@body.to_s(pipe)
+	end
+end
+
+# Read
+class BODY_READ
+	def initialize(val1)
+		@id = val1
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts 'READ:'
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts 'VARIABLE:'
+		for i in 1..pipe
+			print "|  "
+		end
+		puts 'IDENTIFIER: ' + @id.to_s
+	end
+end
+
+# Write
+class BODY_WRITE
+	def initialize(val1)
+		@expr = val1
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts 'WRITE:'
+		@expr.to_s(pipe)
+	end
+end
+
+# Secuenciación
+class BODIES
+	def initialize(val1,val2)
+		@body1 = val1
+		@body2 = val2
+	end
+	def to_s(pipe)
+		@body1.to_s(pipe)
+		@body2.to_s(pipe)
+	end
+end
+
+# Condicional simple
+class IF_THEN
+	def initialize(val1,val2)
+		@exp = val1
+		@body = val2
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "CONDITIONAL STATEMENT:"
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "CONDITION:"
+		@exp.to_s(pipe)
+		for i in 1..pipe-1
+			print "|  "
+		end
+		puts "THEN:"
+		@body.to_s(pipe)
+	end
+end
+
+# Condicional doble
+class IF_THEN_ELSE
+	def initialize(val1,val2,val3)
+		@exp = val1
+		@body1 = val2
+		@body2 = val3
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "CONDITIONAL STATEMENT:"
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "CONDITION:"
+		@exp.to_s(pipe)
+		for i in 1..pipe-1
+			print "|  "
+		end
+		puts "THEN:"
+		@body1.to_s(pipe)
+		for i in 1..pipe-1
+			print "|  "
+		end
+		puts "ELSE:"
+		@body2.to_s(pipe)
+	end
+end
+
+# Expresión
+class EXP
+	def initialize(val)
+		@exp = val
+	end
+	def to_s(pipe)
+		@exp.to_s(pipe)
+	end
+end
+
+# Identificador de variable
+class EXP_ID
+	def initialize(val)
+		@id = val
+	end
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		puts 'IDENTIFIER: ' + @id.to_s
+	end
+end 
+
+# Constante numérica
+class EXP_NUM
+	def initialize(val)	
+		@value = val
+	end
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		puts 'NUMBER: '+@value.to_s
+	end
+end
+
+# Expresión booleana
+class EXP_BOOL
+	def initialize(val)	
+		@value = val
+	end
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		puts 'BOOLEAN: ' + @value.to_s
+	end
+end
+
+# Expresiones binarias
+class DOUBLE_EXP
+	def initialize(val1,val2,val3)	
+		@expr1 = val1
+		@expr2 = val3
+		@oper = val2
+	end
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts 'OPERATION: ' + @oper.to_s
+		@expr1.to_s(pipe)
+		@expr2.to_s(pipe)
+	end
+end
+
+# Expresiones unarias con subexpresión a la izquierda (operador a la derecha)
+class LEFT_EXP
+	def initialize(val1,val2)	
+		@expr = val1
+		@oper = val2
+	end
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts 'OPERATION: '+@oper.to_s
+		@expr.to_s(pipe)
+	end
+end
+
+# Expresiones unarias con subexpresión a la derecha (operador a la izquierda)
+class RIGHT_EXP
+	def initialize(val1,val2)	
+		@expr = val2
+		@oper = val1
+	end
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts 'OPERATION: '+@oper.to_s
+		@expr.to_s(pipe)
+	end
+end
+
+# Iteración con una expresión
+class ONE_COND_ITER
+	def initialize(val1,val2)
+		@expr = val1
+		@body = val2
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "ITERATION STATEMENT:"
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "CONDITION:" 
+		@expr.to_s(pipe)
+		for i in 1..pipe-1
+			print "|  "
+		end
+		puts "THEN:"
+		@body.to_s(pipe)
+	end
+end
+
+# Iteración con dos expresiones
+class ITER
+	def initialize(val1,val2,val3)
+		@expr1 = val1
+		@expr2 = val2
+		@body = val3
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "ITERATION STATEMENT:"
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "LOWER LIMIT:"
+		@expr1.to_s(pipe)
+		for i in 1..pipe-1
+			print "|  "
+		end
+		puts "UPPER LIMIT:" 
+		@expr2.to_s(pipe)
+		for i in 1..pipe-1
+			print "|  "
+		end
+		puts "DO:\n"
+		@body.to_s(pipe)
+	end
+end
+
+
+# Iteración con contador
+class ID_ITER
+	def initialize(val1,val2,val3,val4)
+		@id = val1
+		@expr1 = val2
+		@expr2 = val3
+		@body = val4
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		pipe=pipe+1
+		puts "ITERATION STATEMENT:"
+		for i in 1..pipe
+			print "|  "
+		end
+		puts "IDENTIFIER:" + @id.to_s
+		for i in 1..pipe
+			print "|  "
+		end
+		puts "LOWER LIMIT:" 
+		@expr1.to_s(pipe+1)
+		for i in 1..pipe
+			print "|  "
+		end
+		puts "UPPER LIMIT:"
+		@expr2.to_s(pipe+1)
+		for i in 1..pipe
+			print "|  "
+		end
+		puts "DO:"
+		@body.to_s(pipe+1)
+	end
+end
+
+# Expresiones de tipo lienzo
+class EXP_CANVAS
+	def initialize(val)
+		@canvas = val
+	end
+
+	def to_s(pipe)
+		for i in 1..pipe
+			print "|  "
+		end
+		puts 'CANVAS: '+@canvas.to_s
+	end
+end
---
+++ TablaSimbolos/lexer.rb
@@ -0,0 +1,158 @@
+#!/usr/bin/env ruby
+
+=begin
+
+	@title	Anlisis Lexicográfico del interpretador para el lenguaje Lanscii
+
+	@author	Richard Lares 11-10508
+	@author	Oscar Guillen 11-11264
+
+	@description Código fuente del análisis lexicográfico para el interpretador de Lanscii
+
+=end
+
+class Lexer			# Clase Lexer para realizar el análisis lexicográfico de un archivo
+	def initialize(f)
+		@file=f
+		@token = nil
+		@strings_dic = {"{"=>"LCURLY","}"=>"RCURLY","|"=>"PIPE","%"=>"PERCENTAGE","!"=>"EXCLAMATIONMARK",
+						"@"=>"AT","</>"=>"CANVAS","<\\>"=>"CANVAS","<|>"=>"CANVAS",
+						"<_>"=>"CANVAS","<->"=>"CANVAS","="=>"EQUALS",".."=>"DOUBLEDOT",";"=>"SEMICOLON",
+						"read"=>"READ","write"=>"WRITE","?"=>"QUESTIONMARK", "&"=>"AMPERSAND", "~"=>"TILDE",
+						":"=>"COLON","("=>"LPARENTHESIS",")"=>"RPARENTHESIS", "["=>"LSQUARE", "]"=>"RSQUARE",
+						"+"=>"PLUS","-"=>"MINUS","*"=>"TIMES","/"=>"DIVIDE",
+						"\/\\"=>"AND","\\\/"=>"OR","^"=>"NOT","<"=>"LESS","<="=>"LESSEQUAL",
+						">"=>"GREATER",">="=>"GREATEREQUAL",
+						"\/="=>"NOTEQUAL","#"=>"EMPTYCANVAS","$"=>"DOLLAR","'"=>"APOSTROPHE","true"=>"TRUE",
+						"false"=>"FALSE"}
+	end
+
+	def get_token	# Retorna el arreglo de tokens (o errores lexicográficos)
+		return @token
+	end
+
+	def matching	# Procedimiento de análisis lexicográfico
+
+		# Lectura del contenido del archivo de entrada
+
+		file = File.open(@file,"r")
+		content = file.read
+		file.close
+
+		# Diccionario de tokens con su identificador
+
+
+		correct_program=Array.new	# Arreglo con los strings que se imprimirán cuando un programa es correcto
+		incorrect_program=Array.new	# Arreglo con los strings que se imprimirán cuando un programa es incorrecto
+		incorrect=false # Determina si un programa es incorrecto
+		comment=false 	# Determina si se está dentro de un comentario
+		commentR=0		# Línea del inicio de comentario
+		commentC=0		# Columna del inicio de comentario
+		varMatch=/\{-|-\}|\/=|<\/>|<\\>|<\|>|<_>|<->|<\ >|\/\\|\\\/|<=|>=|\.\.|
+				|([A-Z]|[a-z]|_)([A-Z]|[a-z]|_|[0-9])*|[0-9]+|
+				|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\+|-|\*|\/|\^|<|>|\[|\]|&|~|
+				|#|\$|'|true|false/ 	# Expresión regular para reconocer las palabras reservadas de LANSCII
+		i=1		# Contador de línea
+
+		content.each_line do |x|	# Se itera por las líneas del archivo reconociendo símbolos (o errores)
+			j=0		# Contador de columna
+			s=x.split
+			s.each do |t| # Se itera por el split de la línea para matchear con la regex del lenguaje
+				while x[j]!=t[0] do
+					j+=1
+				end
+				m=t.match(varMatch) 	# Match con la regex del lenguaje
+
+				if !comment 
+					if m == nil	# Se verifican símbolos incorrectos si no está dentro de un comentario
+						incorrect = true
+						for k in 0..(t.size-1)
+							incorrect_program.push("Error: Unexpected character: \""+t[k]+"\" at line: "+i.to_s+", column: "+(j+1+k).to_s)
+						end
+						j+=1
+					else
+						if (n=m.pre_match.match(varMatch) == nil) && (m.pre_match != "")
+							incorrect= true
+							for k in 0..((m.pre_match).size-1)
+								incorrect_program.push("Error: Unexpected character: \""+m.pre_match[k]+"\" at line: "+i.to_s+", column: "+(j+1+k).to_s)
+							end
+						end
+					end
+				end
+				while m do 	# Se itera por el resultado del match hecho para una posición del split de la línea buscando los símbolos del lenguaje,
+							# si no se está dentro de un comentrario
+					if !comment 	
+						if m[0] =~ /\{-/
+							comment=true
+							commentR=i
+							commentC=j+1
+							j+=m[0].size
+
+						elsif m[0] =~ /-}/
+							correct_program.push([eval(':'+@strings_dic[m[0][0]]),m[0][0]])
+							j+=1
+							correct_program.push([eval(':'+@strings_dic[m[0][1]]),m[0][1]])
+							j+=1
+
+						elsif m[0] =~ /<\/>|<\\>|<\|>|<_>|<->|<\ >/
+							correct_program.push([eval(':'+@strings_dic[m[0]]),m[0]])
+							j+=m[0].size
+
+						elsif m[0] =~ /\/=|\/\\|\\\/|<=|>=|\.\.|<|>|\{|\}|\||%|!|@|=|;|read|write|\?|:|\(|\)|\[|\]|&|~|
+							|\+|\-|\*|\/|\^|#|\$|'|true|false/
+							correct_program.push([eval(':'+@strings_dic[m[0]]),m[0]])
+							j+=m[0].size
+
+						elsif m[0] =~ /([A-Z]|[a-z]|_)([A-Z]|[a-z]|_|[0-9])*/
+							correct_program.push([eval(':ID'),m[0]])
+							j+=m[0].size
+
+						elsif m[0] =~ /[0-9]+/
+							correct_program.push([eval(':NUM'),m[0]])
+							j+=m[0].size
+
+						end
+					else
+						if m[0] =~ /-}/
+							comment=false
+						end
+						j+=m[0].size
+					end
+					
+					temp=m.post_match	# Se obtienen los caracteres después del match hecho
+					m=temp.match(varMatch)
+					if !comment
+						if m == nil # Se verifican símbolos incorrectos dentro de una posición del split si no está dentro de un comentario
+							if temp != ""
+								incorrect = true
+								for k in 0..(temp.size-1)
+									incorrect_program.push("Error: Unexpected character: \""+temp[k]+"\" at line: "+i.to_s+", column: "+(j+1+k).to_s)
+								end
+								j+=1
+							end
+						else
+							if (n=m.pre_match.match(varMatch) == nil) && (m.pre_match != "")
+								incorrect= true
+								for k in 0..((m.pre_match).size-1)
+									incorrect_program.push("Error: Unexpected character: \""+m.pre_match[k]+"\" at line: "+i.to_s+", column: "+(j+1+k).to_s)
+								end
+							end
+						end
+					end
+				end
+			end
+			i+=1	
+		end
+		if comment 	# Error si hubo un comentario abierto que nunca fue cerrado
+			puts "Error: Comment section opened but not closed at line: "+commentR.to_s+", column: "+commentC.to_s 
+		else
+			if incorrect 	# Si el programa es incorrecto, se asignan los errores lexicográficos
+				@token = incorrect_program	
+				return false
+			else			# Si el programa es correcto, se asignan los tokens
+				@token = correct_program	
+				return true
+			end
+		end
+	end
+end
