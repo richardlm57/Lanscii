0cd2f5a6c000ca68575a34c4b212bfe1dbbd37c1
richard <richardlm57@gmail.com>
2015-06-14 22:55:06 -0430

    Comentarios

--- TablaSimbolos/symboltable.rb
+++ TablaSimbolos/symboltable.rb
@@ -1,11 +1,25 @@
-class SymbolTable
-	def initialize
+=begin
+
+	@title	Clases para las tablas de símbolos de Lanscii
+
+	@author	Richard Lares 11-10508
+	@author	Oscar Guillen 11-11264
+
+	@description Constructor y métodos definidos para las tablas de símbolos de Lanscii
+
+=end
+
+class SymbolTable 
+
+	# Constructor
+	def initialize	
 		@table=Hash.new
 		@father=nil
 	end
 
 	attr_accessor :table,:father
 
+	# Método para insertar un elemento en la tabla
 	def insert(key,value)
 		result=@table[key]
 		if result == nil
@@ -14,15 +28,23 @@ class SymbolTable
 		end
 		return false
 	end
+
+	# Método para eliminar un elemento de la tabla
 	def delete(key)
 		@table.delete(key)
 	end
+
+	# Método para modificar un elemento de la tabla
 	def update(key,value)
 		insert(key,value)
 	end
+
+	# Método para verificar si un elemento de la tabla existe
 	def contains(key)
 		return @table.has_key?(key)
 	end
+
+	# Método para verificar el valor de un elemento de la tabla
 	def lookup(key)
 		result=@table[key]
 		if result != nil
@@ -34,6 +56,8 @@ class SymbolTable
 		end
 		return nil
 	end
+
+	# Método de impresión de la tabla
 	def to_s
 		a=@table.to_a
 		a.to_s
